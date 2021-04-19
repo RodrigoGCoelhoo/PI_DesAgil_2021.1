4820aeb17e893981e13bbdd7404cfb628897d820
rodrigogcoelhoo <rodrigogc4@al.insper.edu.br>
2021-04-19 16:09:29 -0300

    V2 Q2

--- test/br/edu/insper/desagil/pi/RedeSocialTest.java
+++ test/br/edu/insper/desagil/pi/RedeSocialTest.java
@@ -21,8 +21,8 @@ class RedeSocialTest {
 	
 	@Test
 	void avaliacoesInvalidas() {
-		video.adicionaAvaliacao(usuarioPost, 1);
-		video.adicionaAvaliacao(usuarioPost, 5);
+		video.adicionaAvaliacao(new Usuario("John"), 6);
+		video.adicionaAvaliacao(new Usuario("Snow"), 0);
 		
 		assertEquals(0, video.mediaAvaliacoes());
 	}
@@ -44,23 +44,23 @@ class RedeSocialTest {
 	
 	@Test
 	void tresAvaliacoesValidas() {
-		video.adicionaAvaliacao(new Usuario("Pedro"), 1);
-		video.adicionaAvaliacao(new Usuario("Mara"), 2);
+		video.adicionaAvaliacao(new Usuario("Pedro"), 4);
+		video.adicionaAvaliacao(new Usuario("Mara"), 5);
 		video.adicionaAvaliacao(new Usuario("Ricardo"), 1);
 		
-		assertEquals(1, video.mediaAvaliacoes());
+		assertEquals(3, video.mediaAvaliacoes());
 	}
 	
 	@Test
 	void doisVideos() {
-		video.adicionaAvaliacao(new Usuario("Pedro"), 1);
-		video.adicionaAvaliacao(new Usuario("Mara"), 2);
+		video.adicionaAvaliacao(new Usuario("Pedro"), 4);
+		video.adicionaAvaliacao(new Usuario("Mara"), 5);
 		
 		Video video2 = new Video(2, usuarioPost, produto);
 		
 		video2.adicionaAvaliacao(new Usuario("Ricardo"), 1);
 		
-		assertEquals(2, video.mediaAvaliacoes());
+		assertEquals(5, video.mediaAvaliacoes());
 		assertEquals(1, video2.mediaAvaliacoes());
 		
 	}
